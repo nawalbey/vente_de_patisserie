31cc37541f96c3741541a3a84bd4ce228462e1d6
nawal <nbey34@gmail.com>
2023-11-27 16:49:25 +0100

    login inscription css

--- views/inscription.php
+++ views/inscription.php
@@ -1,40 +1,43 @@
 <?php include_once "../inc/nav_bar.php" ?>
+<div class="class1">
+    <div class="container">
+        <h1>Inscription</h1>
+        <form action="../../vente_de_patisserie/model/db_inscription.php" method="post">
+            <div>
+                <div class="form-group">
+                    <label>nom</label>
+                    <input type="text" class="form-control" name="nom">
+                </div>
+                <div class="form-group">
+                    <label>prenom</label>
+                    <input type="text" class="form-control" name="prenom">
+                </div>
 
-<div class="container">
-    <h1>inscription</h1>
-    <form action="../../vente_de_patisserie/model/db_inscription.php" method="post">
-        <div>
-            <div class="form-group">
-                <label>nom</label>
-                <input type="text" class="form-control" name="nom">
-            </div>
-            <div class="form-group">
-                <label>prenom</label>
-                <input type="text" class="form-control" name="prenom">
-            </div>
+                <div class="form-group">
+                    <label>mot_de_passe</label>
+                    <input type="text" class="form-control" name="mot_de_passe">
+                </div>
 
-            <div class="form-group">
-                <label>mot_de_passe</label>
-                <input type="text" class="form-control" name="mot_de_passe">
-            </div>
+                <div class="form-group">
+                    <label>adresse</label>
+                    <input type="text" class="form-control" name="adresse">
+                </div>
+                <div class="form-group">
+                    <label>numero_telephone</label>
+                    <input type="number" class="form-control" name="numero_telephone">
+                </div>
+                <div class="form-group">
+                    <label>date de naissance</label>
+                    <input type="date" class="form-control" name="date_de_naissance">
+                </div>
 
-            <div class="form-group">
-                <label>adresse</label>
-                <input type="text" class="form-control" name="adresse">
             </div>
-            <div class="form-group">
-                <label>numero_telephone</label>
-                <input type="number" class="form-control" name="numero_telephone">
+            <div class="button1">
+                <button type="submit" id="bouton" class="btn  mt-5 mb-5" name="inscription"
+                    value="submit">Inscription</button>
             </div>
-            <div class="form-group">
-                <label>date de naissance</label>
-                <input type="date" class="form-control" name="date_de_naissance">
-            </div>
- 
-        </div>
-        <button type="submit" id="bouton" class="btn btn-primary mt-5 mb-5" name="inscription"
-            value="submit">Inscription</button>
-    </form>
+        </form>
+    </div>
 </div>
 
 <?php include_once "../inc/footer.php" ?>
--- views/login.php
+++ views/login.php
@@ -3,31 +3,29 @@
 
 include_once "../inc/nav_bar.php"; ?>
 
+<div class="class2">
+    <div>
+        <form class="form1"action="../model/connexion_db.php" method="post">
+            <h1>connexion</h1>
 
-<div class="container">
-    <h1>connexion</h1>
-    <form action="../model/connexion_db.php" method="post">
+            <div class="form-group">
+                <label for="email">Email :</label>
+                <input type="email" class="form-control" id="email" name="email">
+            </div>
 
-        <div class="form-group">
-            <label for="email">Email :</label>
-            <input type="email" class="form-control" id="email" name="email">
-        </div>
+            <div class="form-group">
+                <label for="password">Password :</label>
+                <input type="password" class="form-control" id="password" name="password">
+            </div>
 
-        <div class="form-group">
-            <label for="password">Password :</label>
-            <input type="password" class="form-control" id="password" name="password">
-        </div>
-
-
-        <button type="submit" id="bouton" class="btn btn-primary mt-5 mb-5" name="connexion"
-            value="submit">connexion</button>
-    </form>
-</div>
-<div class="d-flex justify-content-center">
-    <div class="div1">
-        <img src="../asset/img/00217fa56dda538f18086408fafd6ac5.jpg" alt="" width="100">
+            <div class="button2">
+                <button type="submit" id="bouton" class="btn mt-5 mb-5" name="connexion"
+                    value="submit">connexion</button>
+            </div>
+        </form>
     </div>
 </div>
+
 
 
 <?php include_once "../inc/footer.php"; ?>
