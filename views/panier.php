bd3948ca845937ab2f76e3b30fd6a2c41a33d214
nawal <nbey34@gmail.com>
2024-01-11 16:43:13 +0100

    css panier

--- views/panier.php
+++ views/panier.php
@@ -10,37 +10,42 @@ if (!empty($_SESSION['cart'])) {
     $messageVide = "Votre panier est vide";
 }
 ?>
+<div class="class4">
+    <h1>Panier</h1>
+</div>
+<div class="panier">
+    <?php
+    if (isset($gateaux)) {
+        foreach ($gateaux as $key => $gateau) { ?>
+                <div class="">
 
-<h1>panier</h1>
-<?php
-if (isset($gateaux)) {
-    foreach ($gateaux as $key => $gateau) { ?>
-            <div class="">
+                    <div class="card" style="width: 18rem;">
+                        <img src='../asset/img/<?= $gateau["article"]['photo']; ?>' class="card-img-top" alt="verrine">
+                        <div class="card-body">
+                            <p class="prix_gateau">
+                                <?php echo $gateau["article"]['prix']; ?>
+                                €
+                            </p>
+                            <h5 class="card-title">
+                                <?php echo $gateau["article"]['nom_du_gateaux']; ?>
+                            </h5>
+                            <p class="card-text">
+                                <?php echo $gateau["article"]['description']; ?>
+                            </p><br>
+                            <p class="card-text1">
+                                <i class="fa-solid fa-cookie-bite"></i>
+                                <?php echo $gateau["quantite"]; ?>
+                            </p><br>
 
-                <div class="card" style="width: 18rem;">
-                    <img src='../asset/img/<?= $gateau["article"]['photo']; ?>' class="card-img-top" alt="verrine">
-                    <div class="card-body">
-                        <p class="prix_gateau">
-                            <?php echo $gateau["article"]['prix']; ?>
-                            €
-                        </p>
-                        <h5 class="card-title">
-                            <?php echo $gateau["article"]['nom_du_gateaux']; ?>
-                        </h5>
-                        <p class="card-text">
-                            <?php echo $gateau["article"]['description']; ?>
-                        </p><br>
-                        <p class="card-text">
-                            <?php echo $gateau["quantite"]; ?>
-                        </p><br>
-
+                        </div>
                     </div>
                 </div>
-            </div>
-    <?php }
-} else { ?>
-        <p><?= $messageVide ?></p>
-<?php } ?>
+        <?php }
+    } else { ?>
+            <p><?= $messageVide ?></p>
+    <?php } ?>
+</div>
+
 
 <?php include_once "../inc/footer.php" ?>
 
